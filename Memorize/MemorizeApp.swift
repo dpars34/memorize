586eaefc371f17b46fa110a52111dacbe7a9c0fe
Daniel Parsons <dpars@hotmail.co.uk>
2022-04-30 16:36:17 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daniel Parsons on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
